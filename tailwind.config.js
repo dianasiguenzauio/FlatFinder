2e940c01edad907d34fec36f0ea7aeee61cfd9fe
Diana <dsiguenza@krugerschool.edu.ec>
2024-08-21 19:17:55 -0500

    cambios nueva base/ tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./sec/**/*.{js, jsx}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
